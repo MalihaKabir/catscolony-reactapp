9cdabe20aa81eb9722cc018dbdcf452026397146
MalihaKabir <malihakabir01@gmail.com>
2020-01-05 11:49:50 +0600

    Updates

---
+++ precache-manifest.1eb468790d102521f4a8fc86ce91bccd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7674a0610166b68dc2c633bdc5dddfd4",
+    "url": "/catscolony-reactapp/index.html"
+  },
+  {
+    "revision": "d549d7771bc8ff0c8cd6",
+    "url": "/catscolony-reactapp/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "70c939b52b7aad98fd7b",
+    "url": "/catscolony-reactapp/static/css/main.42b82b3a.chunk.css"
+  },
+  {
+    "revision": "d549d7771bc8ff0c8cd6",
+    "url": "/catscolony-reactapp/static/js/2.89721229.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/catscolony-reactapp/static/js/2.89721229.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70c939b52b7aad98fd7b",
+    "url": "/catscolony-reactapp/static/js/main.6c435d54.chunk.js"
+  },
+  {
+    "revision": "13d5b0448af781494274",
+    "url": "/catscolony-reactapp/static/js/runtime-main.862e07a4.js"
+  }
+]);
